ffea8db343bf7e078e805a08ad0a34cf9dd4c3e6
Marcelo de Seixas Cavalcante <pqd.msc@gmail.com>
2021-06-30 13:38:38 -0400

    add componente listaproduto

--- DevMedia/Angular/helloWorld/src/app/app.module.ts
+++ DevMedia/Angular/helloWorld/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ListaProdutoComponent } from './lista-produto/lista-produto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProdutoComponent
   ],
   imports: [
     BrowserModule
