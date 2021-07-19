df799908053a7977c048a42d97038103fdfd7aa4
MarceloCavalcante1981 <44010235+MarceloCavalcante1981@users.noreply.github.com>
2021-07-19 14:29:31 -0400

    corrigido import   HttpClientModule app.module.ts

--- DevMedia/Angular/ex2GRUDlembrete/cliente-lembrete/src/app/app.component.ts
+++ DevMedia/Angular/ex2GRUDlembrete/cliente-lembrete/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cliente-lembrete';
+  titulo = 'GRUD Lembretes com Angular  ';
 }
