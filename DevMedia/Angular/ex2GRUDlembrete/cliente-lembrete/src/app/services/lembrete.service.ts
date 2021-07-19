d3053155d4e3f9b51d1ca65b93f8fb43c25e8e8f
MarceloCavalcante1981 <44010235+MarceloCavalcante1981@users.noreply.github.com>
2021-07-19 13:51:25 -0400

    criado metodo atualizar lembrete

--- DevMedia/Angular/ex2GRUDlembrete/cliente-lembrete/src/app/services/lembrete.service.ts
+++ DevMedia/Angular/ex2GRUDlembrete/cliente-lembrete/src/app/services/lembrete.service.ts
@@ -9,22 +9,25 @@ import { Lembrete } from '../interfaces/lembrete';
 })
 export class LembreteService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getListarLembretes(): Observable<Lembrete[]>{
+  getListarLembretes(): Observable<Lembrete[]> {
     const url = '${environment.lembrete}/lembrete';
     return this.http.get<Lembrete[]>(url);
   }
-  
-  getLembrete(id:number): Observable<Lembrete>{
+  getLembrete(id: number): Observable<Lembrete> {
     const url = '${environment.lembreteApiUrl}/lembrete/';
-    return this.http.post<Lembrete>(url,lembrete);
-  
+    return this.http.post<Lembrete>(url, lembrete);
   }
-  addLembrete(lembrete:Lembrete): Observable<Lembrete>{
+  addLembrete(lembrete: Lembrete): Observable<Lembrete> {
     const url = '${environment.lembreteApiUrl}/lembrete/';
-    return this.http.post<Lembrete>(url,lembrete);
-
-
-  
-}
+    return this.http.post<Lembrete>(url, lembrete);
+  }
+  atualizaLembrete(lembrete: Lembrete): Observable<Lembrete> {
+    const url = `${environment.lembretesApiUrl}/lembrete/${lembrete.id}`;
+    return this.http.put<Lembrete>(url, lembrete);
+  }
+  deletaLembrete(id: number): Observable<Lembrete> {
+    const url = `${environment.lembretesApiUrl}/lembrete/${id}`;
+    return this.http.delete<Lembrete>(url);
+  }
