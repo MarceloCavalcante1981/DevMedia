91926b90d51494d9febdb90475a3693ca3091efa
MarceloCavalcante1981 <44010235+MarceloCavalcante1981@users.noreply.github.com>
2021-07-19 13:28:22 -0400

    import  HTTPCliente no service injeção do httpClinete e add ao modulo

--- DevMedia/Angular/ex2GRUDlembrete/cliente-lembrete/src/app/services/lembrete.service.ts
+++ DevMedia/Angular/ex2GRUDlembrete/cliente-lembrete/src/app/services/lembrete.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from 'selenium-webdriver/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LembreteService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  
 }
