59f9c24095759a995f8fbc352d085c87146f71fb
MarceloCavalcante1981 <44010235+MarceloCavalcante1981@users.noreply.github.com>
2021-07-19 13:44:55 -0400

    criado metodo listar lembrete

--- DevMedia/Angular/ex2GRUDlembrete/cliente-lembrete/src/app/services/lembrete.service.ts
+++ DevMedia/Angular/ex2GRUDlembrete/cliente-lembrete/src/app/services/lembrete.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from 'selenium-webdriver/http';
+import { Observable } from 'rxjs';
+import { Lembrete } from '../interfaces/lembrete';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,12 @@ import { HttpClient } from 'selenium-webdriver/http';
 export class LembreteService {
 
   constructor(private http:HttpClient) { }
+
+  getListarLembretes(): Observable<Lembrete[]>{
+    const url = '${environment.lembrete}/lembrete';
+    return this.http.get<Lembrete[]>(url);
+  }
+ 
 
   
 }
