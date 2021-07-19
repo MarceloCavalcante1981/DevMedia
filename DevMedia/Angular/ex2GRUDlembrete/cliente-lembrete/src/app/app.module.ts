e401616e19bb3bd7e94ff34ec72318424fbf03e8
MarceloCavalcante1981 <44010235+MarceloCavalcante1981@users.noreply.github.com>
2021-07-19 15:08:01 -0400

    created componet msg de erro

--- DevMedia/Angular/ex2GRUDlembrete/cliente-lembrete/src/app/app.module.ts
+++ DevMedia/Angular/ex2GRUDlembrete/cliente-lembrete/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErroMsgComponent } from './compartilhado/erro-msg/erro-msg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErroMsgComponent
   ],
   imports: [
     BrowserModule,
