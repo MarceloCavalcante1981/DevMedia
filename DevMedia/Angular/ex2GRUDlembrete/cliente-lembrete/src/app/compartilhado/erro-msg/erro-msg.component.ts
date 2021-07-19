d57916927f0c87cfa6555ea24f7901bc9d44896c
MarceloCavalcante1981 <44010235+MarceloCavalcante1981@users.noreply.github.com>
2021-07-19 15:23:46 -0400

    atuaalizado classe erro-msc.component.ts

--- DevMedia/Angular/ex2GRUDlembrete/cliente-lembrete/src/app/compartilhado/erro-msg/erro-msg.component.ts
+++ DevMedia/Angular/ex2GRUDlembrete/cliente-lembrete/src/app/compartilhado/erro-msg/erro-msg.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component  } from '@angular/core';
 
 @Component({
   selector: 'app-erro-msg',
   templateUrl: './erro-msg.component.html',
   styleUrls: ['./erro-msg.component.css']
 })
-export class ErroMsgComponent implements OnInit {
+export class ErroMsgComponent  {
+  public error:string;
 
+  setError(error:string, tempo: number = 5000){
+    this.error = error;
+    setTimeout(() => {
+      this.error = null;
+    },tempo);
+  }
   constructor() { }
 
-  ngOnInit(): void {
+  
   }
 
 }
